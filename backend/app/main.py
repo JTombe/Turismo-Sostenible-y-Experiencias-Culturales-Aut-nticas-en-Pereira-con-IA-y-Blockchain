3b50ccbc98fb1483bcb3bdf2ab7cf5c141b4e326
JTombe <134149788+JTombe@users.noreply.github.com>
2025-10-08 17:48:45 -0500

    creaciond e estrucutura basica de proyecto
    creacion basica de la estrucutura del proyecto, actualizacion de gitignore y creacion de dockerignore

---
+++ backend/app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+import requests
+import os
+
+app = FastAPI()
+
+OLLAMA_URL = os.getenv("OLLAMA_API_URL", "http://localhost:11434")
+
+@app.get("/")
+def root():
+    return {"message": "API funcionando correctamente"}
+
+@app.post("/generate")
+def generate(prompt: str):
+    response = requests.post(
+        f"{OLLAMA_URL}/api/generate",
+        json={"model": "llama3", "prompt": prompt},
+        stream=False,
+    )
+    return response.json()
+1
